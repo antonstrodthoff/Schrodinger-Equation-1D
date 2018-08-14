13f046a544d6af3cf199974e6bb397dac205698d
Anton Strodthoff <anton.strodthoff@googlemail.com>
2018-08-14 23:25:07 +0200

    Saving Potential to potential.dat

--- eigenSolver.py
+++ eigenSolver.py
@@ -24,15 +24,28 @@ xPot = rd.xPotential()
 yPot = rd.yPotential()
 delta = (xMax-xMin)/n
 a = 1/(m*delta**2)
-
 xAxis = np.linspace(xMin, xMax, n)
 
 if interType == "polynomial":
     yPotInter = KroghInterpolator(xPot, yPot)
 else:
     yPotInter = interp1d(xPot, yPot, kind=interType)
-    
-e = eigh_tridiagonal(yPotInter(xAxis)+a, np.full(n-1,-1/2*a), select="i", select_range=(fval,lval))
-    
-plt.plot(xAxis, yPotInter(xAxis))
+
+yPotInterString = ""
+
+for (index, poty) in enumerate(yPotInter(xAxis)):
+    yPotInterString += str(xAxis[index])
+    yPotInterString += " "
+    yPotInterString += str(poty)
+    yPotInterString += "\n"
+
+with open("potential.dat", "w") as pot:
+    pot.write(yPotInterString)
+
+(eigenVals, eigenVecs) = eigh_tridiagonal(yPotInter(xAxis)+a, np.full(n-1,-1/2*a), select="i", select_range=(fval,lval))
+
+
+
+plt.plot(xAxis, eigenVecs[:, 2])
+plt.plot(xAxis, yPotInter(xAxis)/100)
 plt.show()
