ffa1c3f6a61e073821752f25f784a9f4ce99fd9e
Anton Strodthoff <anton.strodthoff@googlemail.com>
2018-09-06 20:14:43 +0200

    Even Better Code Structure

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ Created on Thu Aug 16 15:05:31 2018
 
 import eigensolver as es
 
-es.calculate_and_save_results()
+inputpath = input("Please enter path of input file:\n")
+outputpath = input("Where do you want to save the results?\n")
+es.calculate_and_save_results(inputpath, outputpath, save=True, plot=True)
